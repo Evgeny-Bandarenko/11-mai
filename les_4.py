5ec29a29c78fcf59642187ed6985ca6a3c8b4292
Evgeny <104760685+Evgeny-Bandarenko@users.noreply.github.com>
2022-05-11 23:46:44 +0300

    Add files via upload

---
+++ les_4.py
@@ -0,0 +1,56 @@
+import random
+
+
+
+# ls1 = list(range(3, 101, 3))
+# ls2 = list(range(10, 101, 10))
+# print(ls1)
+# print(ls2)
+# ls1 =input()
+# ls2 =input()
+# if ls1 in ls2 or ls2 in ls1:
+#     print("I'm here")
+# else:
+#     print("I'm not here")
+# ls = input()
+# if ls.count('f') == 1:
+#     print(ls.index("f"))
+# elif ls.count("f") > 1:
+#     print(ls.find("f"),ls.rfind("f"))
+
+# str1 =input()
+# if len(str1) % 2 == 0:
+#     print("ewen")
+# elif len(str) % 2 != 0:
+#     print("odd")
+# str1 =int(input("Введите сколько вам лет:"))
+# if str1 < 18 and str1 != 0:
+#     print("Coca cola")
+# elif str1 == 0:
+#     print("Wrong age")
+# else:
+#     print("Beer")
+
+# cube =random.randint(1, 6)
+# print(cube)
+# if cube % 2 == 0:
+#     choco = int(input("Сколько шоколадок"))
+#     today = input("День недели")
+#     off_days =["Sun", "Sat"]
+#     if today in off_days: # Алексу 3
+#         if choco - 3 >= 2:
+#             print("Go to Paul")
+#     else:
+#         if choco - 2 >= 3:
+#             print("Go to Alex")
+# else:
+#     print("Stay home(")
+# str1 =int(input())
+# if str1 <= 2 or str1 == 12:
+#     print("Зима")
+# elif 2 <= str1 <= 5:
+#     print("Весна")
+# elif 5 <= str1 <= 8:
+#     print("Лето")
+# elif 8 <= str1 <= 11:
+#     print("Осень")
