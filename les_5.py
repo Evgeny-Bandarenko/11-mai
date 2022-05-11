c6ca9f745d64f4c675db2ec20301631d3da01c22
Evgeny <104760685+Evgeny-Bandarenko@users.noreply.github.com>
2022-05-11 23:47:06 +0300

    Add files via upload

---
+++ les_5.py
@@ -0,0 +1,97 @@
+# nums = list(range(1, 101))
+# for i in nums:
+#     print(i)
+
+# text = input("Enter some text:")
+# for i in text:
+#     print(i)
+
+# grocery = ['bread', 'milk', 'butter']
+# for index, item in enumerate(grocery):
+#     print(index, item, sep="-->")
+
+# text = input()
+# for index, item in enumerate(text):
+#     print(index, item)
+
+# nums = int(input())
+# numbers = int(input())
+# for i in range(min(nums, numbers), max(nums, numbers)+1):
+#     print(i)
+
+# a = int(input('number a'))
+# b = int(input('number b'))
+# if a > b:
+#     for i in range(b, a+1):
+#         print(i)
+# elif b > a:
+#     for i1 in range(a, b+1):
+#         print(i1)
+# print('end')
+
+# nums = int(input())
+# numbers = int(input())
+# for i in range(min(nums, numbers), max(nums, numbers)+1):
+#     if i % 2 == 0:
+#         print(i)
+
+# a = int(input('number a'))
+# b = int(input('number b'))
+# if a > b:
+#     for i in range(b, a+1):
+#         print(i)
+# elif b > a:
+#     for i1 in range(a, b+1):
+#         print(i1)
+# print('end')
+
+# nums = input()
+# for i in nums:
+#     print(i*2)
+
+# nums = int(input())
+# numbers = int(input())
+# for i in range(min(nums, numbers), max(nums, numbers)+1):
+#     if i % 2 == 0 and i % 7 == 1:
+#         print(i)
+
+# nums = [int(i) for i in input().split()]
+# print(nums)
+
+# a = int(input())
+# b = int(input())
+# sun_nums = 0
+# for i in range(a, b+1):
+#     # sun_nums = sun_nums+i
+#     sun_nums += i
+# print(sun_nums)
+
+# a = int(input())
+# b = int(input())
+# sun_nums = 0
+# for i in range(a, b+1):
+#     # sun_nums = sun_nums+i
+#     sun_nums += i**2
+# print(sun_nums)
+
+# a = int(input())
+# b = int(input())
+# sum_nums = sum(i for i in range(a, b+1))
+# print(sum_nums)
+
+# a = int(input())
+# b = int(input())
+# sum_nums = sum(i**2 for i in range(a, b+1))
+# print(sum_nums)
+#
+# nums = input().split()
+# nums2 = []
+# for i in nums:
+#     if int(i) % 2 ==1:
+#         nums2.append(int(i)**2)
+# print(nums2)
+
+# nums = [int(i) for i in input().split() if int(i) > 5]
+# print(nums)
+
+
