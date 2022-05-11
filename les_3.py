2892db7a78586360c3f37f77bb1a7158479460c6
Evgeny <104760685+Evgeny-Bandarenko@users.noreply.github.com>
2022-05-11 23:46:18 +0300

    Add files via upload

---
+++ les_3.py
@@ -0,0 +1,64 @@
+# Ls1 = [1, 4.5, True, "word", [1, 2, 3, 4]]
+# Ls2 = list((3, 4, 5, 6))
+# words = input().split()
+# print(words)
+# animals = ['cat', 'dog', 'rabbit']
+# animals.append('pig')
+# print(animals)
+# animals = ['cat', 'dog']
+# numbers = [1, 2, 3]
+# print(animals + numbers)
+# animals = ['cat', 'dog']
+# animals[1] = 3
+# print(animals)
+# colors = input().split()
+# colors = colors[::-1]
+# colors.append(more_color)
+# print(colors[::-1])
+# number = [0, 0, 0]
+# number.append(1)
+# number[0] = 1
+# print(number)
+# zero = [0]
+# one = [1]
+# print(one + zero*3 + one)
+# numbers = ["word", "hi", "lol"]
+# # del numbers[2]
+# # numbers.clear()
+# # numbers.remove("hi")
+# del_ilement = numbers.pop(-1)
+# print(del_ilement)
+# print(numbers)
+# r1 = random.randrange(1, 21)
+# print(r1)
+# r2 = random.randint(1, 20)
+# print(r2)
+# r3 = random.random()
+# print(r3)
+# systems = ['Windows', 'Linux']
+# systems.insert(1, 'macOS')
+# print(systems)
+# ystems = ['Windows', 'macOS', 'Linux']
+# systems = systems[::-1]
+# systems.reverse()
+# print(systems)
+# vowels = ['a', 'e', 'i', 'o', 'i', 'u']
+# vowels.sort(reverse=True)
+# nums =[1, 2, 3]
+# new = nums.copy()
+# new[1] = "a"
+# print(new)
+# print(nums)
+# numb = input().split()
+# num = input()
+# print(num in numb)
+# numbers = [1, 2, 3, 4, 5]
+# new_numbers = numbers.copy()
+# del new_numbers[2:]
+# print(numbers)
+# print(new_numbers)
+# nums = [1, 2, 3, 4, 5]
+# max_item = max(nums)
+# min_item = min(nums)
+# ind_max_item
+
